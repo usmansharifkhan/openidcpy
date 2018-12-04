2036708903f1821fa91d10c4f8b6769c9bd3125b
Usman Shahid <usman.shahid@Intechww.com>
2018-12-04 12:26:38 +0500

    Added initial implementation

---
+++ README.md
@@ -0,0 +1,54 @@
+## Openidcpy
+A package that implements the Authorization Flow client for the OpenID Connect relying party. The package cane be used to fetch OpenID Connect tokens using Authorization Flow and validate them using JWKS. The client has been implemented with KeyCloak as the OpenID Connect Authorizaiton Provider.
+
+#### Usage
+
+The only class of use if `OidcClient` which can be imported in code as:
+
+```python
+from openidcpy.client import OidcClient
+```
+
+You'd need to initialize this client as follows:
+
+```python
+discovery_url = 'The .well_known discovery endpoint'
+client_id = 'The client id assigned to the RP'
+client_secret = 'The client secret assigned to RP if the client is private, otherwise None'
+client = OidcClient(discovery_url=discovery_url, client_id=client_id, client_secret=client_secret)
+```
+
+The client basically exposes 3 methods that, if called in sequence, complete the Authorization Flow. The first call is to create the authorization code url:
+
+```python
+response_type = 'code'  # Always `code` for Authorization Flow
+redirect_uri = 'uri where the relying party is running'
+scopes = ['openid', 'email', 'profile'] # Array of requested scopes
+state = 'Some random generated value that is returned in the redirect, prevents csrf'
+url = client.get_auth_url(response_type=response_type, redirect_uri=redirect_uri, scopes=scopes, state=state)
+```
+
+You'll need to redirect your application to the above url. The redirection will cause the auth server's login page to be displayed. Once the user logs in, the browser is redirected to the `redirect_uri` passed into the above call with an authorization code. You'll need to exchange the code with the JWT token as follows:
+
+```python
+url = 'redirect_uri + query params, basically the endpoint on which the auth server redirected'
+redirect_uri = 'uri where the relying party is running'
+scopes = ['openid', 'email', 'profile'] # Array of requested scopes
+state = 'Same value from the previous request'
+tokens = client.get_tokens_from_code(url=url, redirect_uri=redirect_uri, scopes=scopes, state=state)
+```
+
+Now, depending on the scopes you used the `tokens` dictionary can have either `access_token`, the `id_token` or both.
+
+The next step is to validate these tokens, for which the client provides the following method:
+
+```python
+id_token = tokens['id_token']
+claims = client.validate_jwt(id_token)
+```
+The method validates the signature of the token, the audience (whether the token was intended for you) and the expiration of the token.
+The `claims` dictionary will contain the decoded contents of the token. Based on the scopes you specified, it can contain the `email` or `preferred_username`.
+
+#### Contributions
+
+Contributions are strongly encouraged, since this is just a basic implementation of the Authorization and there's a lot more to OpenId Connect that can be added here. I will try to add the rest whenever I find the time but if I can't, feel free to add it yourself.
---
+++ tests/test_client.py
@@ -0,0 +1,216 @@
+import base64
+import json
+import sys
+import time
+import unittest
+
+import requests
+from mock import Mock
+from requests import Response
+from openidcpy import client as client_file
+
+from openidcpy.client import OidcClient
+
+sys.path.insert(0, '../superset/custom')
+
+WELL_KNOWN = '{\"issuer\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\",\"authorization_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/auth\",\"token_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/token\",\"token_introspection_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/token\/introspect\",\"userinfo_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/userinfo\",\"end_session_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/logout\",\"jwks_uri\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/certs\",\"check_session_iframe\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/protocol\/openid-connect\/login-status-iframe.html\",\"grant_types_supported\":[\"authorization_code\",\"implicit\",\"refresh_token\",\"password\",\"client_credentials\"],\"response_types_supported\":[\"code\",\"none\",\"id_token\",\"token\",\"id_token token\",\"code id_token\",\"code token\",\"code id_token token\"],\"subject_types_supported\":[\"public\",\"pairwise\"],\"id_token_signing_alg_values_supported\":[\"RS256\"],\"userinfo_signing_alg_values_supported\":[\"RS256\"],\"request_object_signing_alg_values_supported\":[\"none\",\"RS256\"],\"response_modes_supported\":[\"query\",\"fragment\",\"form_post\"],\"registration_endpoint\":\"http:\/\/localhost:8080\/auth\/realms\/teamplay\/clients-registrations\/openid-connect\",\"token_endpoint_auth_methods_supported\":[\"private_key_jwt\",\"client_secret_basic\",\"client_secret_post\",\"client_secret_jwt\"],\"token_endpoint_auth_signing_alg_values_supported\":[\"RS256\"],\"claims_supported\":[\"sub\",\"iss\",\"auth_time\",\"name\",\"given_name\",\"family_name\",\"preferred_username\",\"email\"],\"claim_types_supported\":[\"normal\"],\"claims_parameter_supported\":false,\"scopes_supported\":[\"openid\",\"profile\",\"phone\",\"offline_access\",\"email\",\"address\"],\"request_parameter_supported\":true,\"request_uri_parameter_supported\":true,\"code_challenge_methods_supported\":[\"plain\",\"S256\"],\"tls_client_certificate_bound_access_tokens\":true}'
+KEYS = '{\"keys\":[{\"kid\":\"gUp7OURQn7iAbgzJXfJimdz1lBlcIgxkPhthkE9h9B8\",\"kty\":\"RSA\",\"alg\":\"RS256\",\"use\":\"sig\",\"n\":\"h3VxiZbdsK1jlz89TpnOjmg63WULdKxo9UnvOel-eIlJ8zzHQsrvNinVB4g-h9GSEG0VrV1ZY-pkqHfPfar0BjJ_LUdf_uFQLmxUMNk3evFfcVna6FClDA094D9iCRVxtOCTsz_-WjfZo0yBEv8FTsbKluXflNHxb72rqUHKpezIBAXXyLHCN2N3Qt7eeH6f57k4MpQutqibwZHeVEryUyRHN4P1fkf98NvnakjWe4LdXiK2CA_kf3oZ40LLDZWCOiKPh25SxW6ZP1ZHdDtlZZNTL54K_xTE3mvHL9zyB55-pLAr_gHUhENIR3I3AFsSshDvAhPL4JaYre3KedAviw\",\"e\":\"AQAB\"}]}'
+
+
+def ordered(obj):
+  if isinstance(obj, dict):
+    return sorted((k, ordered(v)) for k, v in obj.items())
+  if isinstance(obj, list):
+    return sorted(ordered(x) for x in obj)
+  else:
+    return obj
+
+
+def close():
+  pass
+
+
+class TestOidcClient(unittest.TestCase):
+  def test_discover(self):
+    def get(url, verify):
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      if url == 'http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration':
+        response.json = lambda: json.loads(WELL_KNOWN)
+      elif url == 'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/certs':
+        response.json = lambda: json.loads(KEYS)
+      else:
+        self.fail('Unexpected url: {}'.format(url))
+      return response
+
+    requests.get = Mock(side_effect=get)
+    client = OidcClient(
+        discovery_uri='http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration',
+        client_id='asdf')
+
+    client._discover()
+    well_known = json.loads(WELL_KNOWN)
+    self.assertTrue(
+        ordered(client.well_known) == ordered(well_known))
+    self.assertEqual(client.auth_uri, well_known['authorization_endpoint'])
+    self.assertEqual(client.token_uri, well_known['token_endpoint'])
+    jwks = json.loads(KEYS)
+    self.assertTrue(
+        ordered(client.certs[jwks['keys'][0]['kid']]) == ordered(
+            jwks['keys'][0]))
+
+  def test_get_auth_url(self):
+    def get(url, verify):
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      if url == 'http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration':
+        response.json = lambda: json.loads(WELL_KNOWN)
+      elif url == 'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/certs':
+        response.json = lambda: json.loads(KEYS)
+      else:
+        self.fail('Unexpected url: {}'.format(url))
+      return response
+
+    requests.get = Mock(side_effect=get)
+    client = OidcClient(
+        discovery_uri='http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration',
+        client_id='asdf')
+    url = client.get_auth_url('code', 'http://yourwebsite.com/redirect',
+                              ['abc', 'def'], 'wyoming')
+    self.assertTrue(
+        'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/auth' in url)
+    self.assertTrue('scope=abc+def' in url)
+    self.assertTrue('state=wyoming' in url)
+    self.assertTrue(
+        'redirect_uri=http%3A%2F%2Fyourwebsite.com%2Fredirect' in url)
+    self.assertTrue('response_type=code' in url)
+    self.assertTrue('client_id=asdf' in url)
+
+  def test_get_auth_url_single_scope(self):
+    def get(url, verify):
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      if url == 'http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration':
+        response.json = lambda: json.loads(WELL_KNOWN)
+      elif url == 'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/certs':
+        response.json = lambda: json.loads(KEYS)
+      else:
+        self.fail('Unexpected url: {}'.format(url))
+      return response
+
+    requests.get = Mock(side_effect=get)
+    client = OidcClient(
+        discovery_uri='http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration',
+        client_id='asdf')
+    url = client.get_auth_url('code', 'http://yourwebsite.com/redirect',
+                              'abc', 'wyoming')
+    self.assertTrue(
+        'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/auth' in url)
+    self.assertTrue('scope=abc' in url)
+    self.assertTrue('state=wyoming' in url)
+    self.assertTrue(
+        'redirect_uri=http%3A%2F%2Fyourwebsite.com%2Fredirect' in url)
+    self.assertTrue('response_type=code' in url)
+    self.assertTrue('client_id=asdf' in url)
+
+  def test_get_tokens_from_code(self):
+    url = 'http://localhost:8088/oidc-authorized?state=mystate&code=mycode'
+
+    def get(url, verify):
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      if url == 'http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration':
+        response.json = lambda: json.loads(WELL_KNOWN)
+      elif url == 'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/certs':
+        response.json = lambda: json.loads(KEYS)
+      else:
+        self.fail('Unexpected url: {}'.format(url))
+      return response
+
+    def post(url, data, headers, verify):
+      self.assertEqual(url,
+                       'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/token')
+      self.assertTrue(ordered(data) == ordered(
+          {'code': 'mycode', 'scope': 'abc def',
+           'grant_type': 'authorization_code', 'client_id': 'thetick',
+           'redirect_uri': 'http://yourwebsite.com/redirect'}))
+      self.assertTrue(ordered(headers) == ordered(
+          {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+           'Accept': 'application/json',
+           'Authorization': 'Basic dGhldGljazphcnRodXI='}))
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      response.json = lambda: {'access_token': 'I grant thy access'}
+      return response
+
+    requests.get = Mock(side_effect=get)
+    requests.post = Mock(side_effect=post)
+    client = OidcClient(
+        discovery_uri='http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration',
+        client_id='thetick', client_secret='arthur')
+    token = client.get_tokens_from_code(url, 'http://yourwebsite.com/redirect',
+                                        ['abc', 'def'], 'mystate')
+    self.assertTrue('access_token' in token)
+    self.assertTrue(token['access_token'], 'I grant thy access')
+
+  def test_validate_jwt(self):
+
+    def get(url, verify):
+      response = Response()
+      response.status_code = 200
+      response.close = close
+      if url == 'http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration':
+        response.json = lambda: json.loads(WELL_KNOWN)
+      elif url == 'http://localhost:8080/auth/realms/teamplay/protocol/openid-connect/certs':
+        response.json = lambda: json.loads(KEYS)
+      else:
+        self.fail('Unexpected url: {}'.format(url))
+      return response
+
+    requests.get = Mock(side_effect=get)
+    client = OidcClient(
+        discovery_uri='http://localhost:8080/auth/realms/teamplay/.well-known/openid-configuration',
+        client_id='unittest')
+
+    test_header = {
+      'kid': 'gUp7OURQn7iAbgzJXfJimdz1lBlcIgxkPhthkE9h9B8',
+      'alg': 'RS256'
+    }
+    test_payload = {
+      'iss': 'keycloak',
+      'sub': 'testing',
+      'aud': 'unittest',
+      'iat': int(time.time()),
+      'nbf': int(time.time()),
+      'exp': int(time.time()) + 60,
+      'jti': 'my-jwt',
+      'username': 'bushido'
+    }
+
+    token_str = '{}.{}.{}'.format(base64.b64encode(json.dumps(test_header)),
+                                  base64.b64encode(json.dumps(test_payload)),
+                                  'my-signature')
+
+    def get_jwt(key, token):
+      self.assertEqual(token, token_str)
+
+      class TestTokenBody:
+        claims = None
+
+      token_body = TestTokenBody()
+      token_body.claims = json.dumps(test_payload)
+      return token_body
+
+    client_file._new_jwt = get_jwt
+    claims = client.validate_jwt(token_str)
+
+    self.assertTrue(ordered(claims) == ordered(test_payload))
+
+
+if __name__ == '__main__':
+  unittest.main()
