1dcfb2798114f8ef2bc546850fa93903cec67369
Usman Shahid <usman.shahid@Intechww.com>
2018-12-11 12:57:26 +0500

    Added project link

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="openidcpy",
     version='0.1',
-    description='A package that implements the RP auth flow for OpenId Connect',
+    description='A package that implements the Relying Party Authorization Code Flow for OpenId Connect',
     license='Apache 2.0',
     author='Usman Shahid',
     author_email='usman.shahid@intechww.com',
+    url='https://github.com/intech-iiot/openidcpy',
     tests_require=['mock==2.0.0'],
     install_requires=[
       'requests==2.20.0',
