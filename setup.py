d51e9ad33a62a1a7a02a3ec3f4a23e102e9076fc
Usman Shahid <usman.shahid@Intechww.com>
2018-12-11 13:09:54 +0500

    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
 
 setup(
     name="openidcpy",
     version='0.1',
     description='A package that implements the Relying Party Authorization Code Flow for OpenId Connect',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='Apache 2.0',
     author='Usman Shahid',
     author_email='usman.shahid@intechww.com',
