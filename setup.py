fbd82a5494e3b765a5583b16ca01c5a0d96079ee
usmansharifkhan <usman.khan@intechww.com>
2019-04-18 18:11:04 +0500

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name="openidcpy",
-    version='0.7',
+    version='0.8',
     description='A package that implements the Relying Party Authorization Code Flow for OpenId Connect',
     long_description=long_description,
     long_description_content_type='text/markdown',
